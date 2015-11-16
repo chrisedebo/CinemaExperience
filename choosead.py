5ccc23f2b88d5bbe08c793d4c1a22671dcf3dc11
U-Laptop\Chris <chrisedebo@gmail.com>
2015-11-16 23:40:31 +0000

    Changed video config format

--- choosead.py
+++ choosead.py
@@ -4,15 +4,41 @@ import os
 import re
 import random
 
-prestr = 'pre-'
-poststr = 'post-'
-preposttypes = {'cine': 2, 'ad': 3 }
-videotypes = {'trailer': 1}
-dirtypes = {'cine': 'ads', 'ad': 'ads', 'trailer': 'ads'}
+#Video types, directories, count of type and prefix for two part 
+videos = [{'type':'PDOpen',   'dir' : 'PearlandDeanIdents', 'Count' : 1, 'Prefix': 'Opening_'},
+        [{'type':'Ads',       'dir' : 'Adverts',            'Count' : 5, 'Prefix': None},
+        [{'type':'PDClose',   'dir' : 'PearlandDeanIdents', 'Count' : 1, 'Prefix': 'Close_'},
+        [{'type':'CineID',    'dir' : 'CinemaIdents',       'Count' : 1, 'Prefix': None},
+        [{'type':'InHouseAds','dir' : 'InHouseAdverts',     'Count' : 2, 'Prefix': None},
+        [{'type':'Prev',      'dir' : 'Previews',           'Count' : 1, 'Prefix': None},
+        [{'type':'Tr',        'dir' : 'Trailers',           'Count' : 1, 'Prefix': None},
+        [{'type':'CineP',     'dir' : 'CinemaPolicy',       'Count' : 1, 'Prefix': None},
+        [{'type':'FP',        'dir' : 'FeaturePresentation','Count' : 1, 'Prefix': None},
 
-def selectrandomfiles(searchstr,noofitems,dir):
+
+
+
+#Video types and directories they are stored in
+videodirtypes = {'PDOpen': 'PearlandDeanIdents', 'Ads': 'Adverts', 
+    'PDClose': 'PearlandDeanIdents', 'CineId': 'CinemaIdents', 'InHouseAds': 'InHouseAdverts',
+    'Prev': 'Previews', 'Tr': 'Trailers', 'CineP': 'CinemaPolicy', 'FP': 'FeaturePresentation'}
+
+#Video order count
+videocount = [{'PDOpen': 1}, 'Ads': 5, 'PDClose': 1, 'CineId': 1, 'InHouseAds': 2, 
+    'Prev': 1, 'Tr': 3, 'CineP': 1, 'FP': 1}]
+
+#Videos with a prefix for open/close
+prefixtypes = {'PDopen': 'Opening_', 'PDClose': 'Closing_'}
+
+
+def selectrandomfiles(vdir,vcount,vprefix = None):
 	"Select random number of files based on searchstr in directory dir"
-	randomfiles = [f for f in os.listdir(dir) if re.match(searchstr + '*',f)]
+    if vprefix == None
+        searchstr = '*'
+    else
+        searchstr = vprefix + '*'
+
+	randomfiles = [f for f in os.listdir(dir) if re.match(searchstr,f)]
 	selectedrandomfiles = random.sample(randomfiles,noofitems)
 
 	return selectedrandomfiles
@@ -21,31 +47,10 @@ def printvideos(videos):
 	for videotype, videolist in videos.items():
 		print("=====" + videotype + "=====")
 		print("\n".join(videolist))
-#Get Pre videos
-prevideos = {}
-for tstr, items in preposttypes.items():
-	prevideos[tstr] = selectrandomfiles(prestr + tstr,items,dirtypes[tstr])
 
-print("================Pre Videos================")
-printvideos(prevideos)
+def CreatePlaylist()
+    Print "Creating Playlist"
 
-#get other video types
-othervideos = {}
-for tstr, items in videotypes.items():
-	othervideos[tstr] = selectrandomfiles(tstr,items,dirtypes[tstr])
-
-print("================Other Videos================")
-printvideos(othervideos)
-
-#Select post videos to match Pre
-postvideos = {}
-for tstr, vidlist in prevideos.items():
-	if type(vidlist) is list:
-		postvideos[tstr] = [v.replace(prestr,poststr) for v in vidlist]
-	else :
-		postvideos[tstr] = vidlist.replace(prestr,poststr)
-
-print("================Post Videos================")
-printvideos(postvideos)
-
-
+def choosevideos(videotype):
+    searchstr = openclosetypes.get(videotype)
+    
---
+++ testdict.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+d = {'key1': 'value1'}
+
+print(d.get('key1'))
+print(d.get('key2'))
+
